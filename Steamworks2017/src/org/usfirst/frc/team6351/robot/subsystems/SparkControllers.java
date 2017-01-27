3375ddaad3f39070cff7d04cb84735ea0adb53fe
Davis Carlson <bigdavisc@shaw.ca>
2017-01-27 12:27:43 -0700

    Added Command and Subsystem for SPARK Controllers

---
+++ Steamworks2017/src/org/usfirst/frc/team6351/robot/subsystems/SparkControllers.java
@@ -0,0 +1,24 @@
+package org.usfirst.frc.team6351.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class SparkControllers extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+	
+	public void setController(int controllerPosition, double speed) {
+		
+	}
+	
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
