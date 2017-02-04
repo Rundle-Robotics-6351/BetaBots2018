c2ac4ff5aa6b8b44e6e217b89dc99105efc71471
Rundle-Robotics-6351 <srhigh@rundle.ab.ca>
2017-02-04 11:32:54 -0700

    Added Command to Activate SPark Motors

--- Steamworks2017/src/org/usfirst/frc/team6351/robot/commands/ActivateSparkTest.java
+++ Steamworks2017/src/org/usfirst/frc/team6351/robot/commands/ActivateSparkTest.java
@@ -37,5 +37,6 @@ public class ActivateSparkTest extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	Robot.sparks.setSpinner(0.0);
     }
 }
