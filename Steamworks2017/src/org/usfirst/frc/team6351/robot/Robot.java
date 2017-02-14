893d97993aad6cda2528fb9ab7bbd5d5aa74d8b5
Rundle-Robotics-6351 <srhigh@rundle.ab.ca>
2017-02-14 15:43:19 -0700

    Set USB Cameras to use 30 FPS

--- Steamworks2017/src/org/usfirst/frc/team6351/robot/Robot.java
+++ Steamworks2017/src/org/usfirst/frc/team6351/robot/Robot.java
@@ -88,6 +88,8 @@ public class Robot extends IterativeRobot {
         usbCamera2 = new UsbCamera("USB Camera 1", 1);
         usbCamera1.setResolution(RobotMap.IMG_WIDTH, RobotMap.IMG_HEIGHT);
         usbCamera2.setResolution(RobotMap.IMG_WIDTH, RobotMap.IMG_HEIGHT);
+        usbCamera1.setFPS(30);
+        usbCamera2.setFPS(30);
         
         mjpegServer1 = new MjpegServer("serve_USB Camera 0", 1181);
         mjpegServer1.setSource(usbCamera1);
