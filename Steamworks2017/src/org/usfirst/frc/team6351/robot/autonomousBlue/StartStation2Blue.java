7cebf48ff5f1a63fde3442ff9934a14ef0dc41e3
RundleRobotics <RundleRobotics@LAPTOP-J0OJVLCL>
2017-04-06 17:13:01 -0600

    Created Package for Blue Alliance Auto. and Change command names

---
+++ Steamworks2017/src/org/usfirst/frc/team6351/robot/autonomousBlue/StartStation2Blue.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team6351.robot.autonomousBlue;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.usfirst.frc.team6351.robot.commands.*;
+
+/**
+ *
+ */
+public class StartStation2Blue extends CommandGroup {
+
+    public StartStation2Blue() {
+    	
+    	addSequential(new AutoDriveStraight(0.3,2));
+    	Timer.delay(13);
+//    	addSequential(new AutoDriveStraight(-0.6,1));
+//    	addSequential(new AutoTurn(-90));
+//    	addSequential(new AutoDriveStraight(0.6,2));
+//    	addSequential(new AutoTurn(90));
+//    	addSequential(new AutoDriveStraight(0.6,2));
+//    	addSequential(new AutoTurn(-135));
+//    	addSequential(new AutoDriveStraight(0.3,1.5));
+//    	addSequential(new ActivateShooter());
+    	// Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    }
+}
