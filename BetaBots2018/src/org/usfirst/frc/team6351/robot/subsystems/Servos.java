28c7d3e03230c80fb22c082f853008dfeab2188f
Rundle Robotics <Rundle Robotics@robotics.RUNDLE.ADS>
2017-11-13 15:33:13 -0700

    Files From Build

---
+++ BetaBots2018/src/org/usfirst/frc/team6351/robot/subsystems/Servos.java
@@ -0,0 +1,36 @@
+package org.usfirst.frc.team6351.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Servos extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	
+	public Servo gateServo;
+	
+	public Servos() {
+		 gateServo = new Servo(5);
+	}
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+
+//    public void openGate() {
+//    	gateServo.set(0.5);
+//    }
+//    public void closeGate() {
+//    	gateServo.set(0);
+//    }
+    public double getServoPosition() {
+    	return gateServo.getPosition();
+    }
+}
+
