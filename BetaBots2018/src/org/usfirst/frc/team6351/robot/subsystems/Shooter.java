f0c3f9091bc98a043be4712d2ce02f0c799f16e5
RundleClassmate <srhigh@rundle.ab.ca>
2017-11-20 12:09:15 -0700

    Added more comments.

--- BetaBots2018/src/org/usfirst/frc/team6351/robot/subsystems/Shooter.java
+++ BetaBots2018/src/org/usfirst/frc/team6351/robot/subsystems/Shooter.java
@@ -17,16 +17,19 @@ public class Shooter extends Subsystem
 	
 	public Shooter()
 	{
+		//Assigns shooter wheel to spot on the PCB.
 		shooterWheel = new Spark(6);
 	}
 	
 	public void fire()
 	{
+		//When this is activated, the motor spins at the same magnitude as set in RobotMap.
 		shooterWheel.set(RobotMap.Spark_Controller_Shooter);
 	}
 	
 	public void stop()
 	{
+		//When this is activated, the motor stops doing stuff.
 		shooterWheel.set(0);
 	}
 	
