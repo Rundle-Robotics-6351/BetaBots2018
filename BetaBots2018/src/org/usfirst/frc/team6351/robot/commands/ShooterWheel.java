5748287105ebcf5907a7804f4c658bbb99d5dbaa
RundleClassmate <srhigh@rundle.ab.ca>
2017-11-20 11:58:17 -0700

    Added shooter command functionality.

--- BetaBots2018/src/org/usfirst/frc/team6351/robot/commands/ShooterWheel.java
+++ BetaBots2018/src/org/usfirst/frc/team6351/robot/commands/ShooterWheel.java
@@ -2,28 +2,32 @@ package org.usfirst.frc.team6351.robot.commands;
 
 import org.usfirst.frc.team6351.robot.Robot;
 import org.usfirst.frc.team6351.robot.RobotMap;
+import org.usfirst.frc.team6351.robot.subsystems.Shooter;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
-public class ShooterWheel extends Command {
+public class ShooterWheel extends Command 
+{
 
     public ShooterWheel() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-    	requires(Robot.servos);
+    	requires(Robot.shooter);
     }
 
     // Called just before this Command runs the first time
-    protected void initialize() {
+    protected void initialize() 
+    {
+    	Robot.shooter.fire();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	
-    	double rightTrigger = Robot.oi.controllerDriverAxisValue(RobotMap.Controller1_Right_Trigger);
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -32,7 +36,9 @@ public class ShooterWheel extends Command {
     }
 
     // Called once after isFinished returns true
-    protected void end() {
+    protected void end() 
+    {
+    	Robot.shooter.stop();
     }
 
     // Called when another command which requires one or more of the same
