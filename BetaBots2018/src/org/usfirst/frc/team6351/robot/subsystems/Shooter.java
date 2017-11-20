7b96e09658bd3f747abfdc5058bba60adee9851a
RundleClassmate <srhigh@rundle.ab.ca>
2017-11-20 11:42:22 -0700

    Added shooter subsystem and command.

---
+++ BetaBots2018/src/org/usfirst/frc/team6351/robot/subsystems/Shooter.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc.team6351.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.*;
+import org.usfirst.frc.team6351.robot.RobotMap;
+
+/**
+ *
+ */
+public class Shooter extends Subsystem 
+{
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+	public Spark shooterWheel;
+	
+	public Shooter()
+	{
+		
+	}
+	
+	public void fire()
+	{
+		shooterWheel.set(RobotMap.Spark_Controller_Shooter);
+	}
+	
+	public void stop()
+	{
+		shooterWheel.set(0);
+	}
+	
+    public void initDefaultCommand() 
+    {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
