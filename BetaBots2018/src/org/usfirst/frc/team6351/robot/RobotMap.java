298d94c357e2a788e19397973a2b698f0bfa4af4
Davis Carlson <bigdavisc@shaw.ca>
2017-11-30 16:14:12 -0700

    Slowed down Auto Turn

--- BetaBots2018/src/org/usfirst/frc/team6351/robot/RobotMap.java
+++ BetaBots2018/src/org/usfirst/frc/team6351/robot/RobotMap.java
@@ -23,7 +23,7 @@ public class RobotMap {
     // public static int rangefinderPort = 1;
     // public static int rangefinderModule = 1;
 	public static final double Tank_Drive_Scaling_Teleop = 0.6;
-	public static final double Drive_Scaling_Auto = 0.6;
+	public static final double Drive_Scaling_Auto = 0.4;
 	public static final double Tank_Drive_Precision_Scaling_Teleop = 0.2;
 	
 	public static final double GTA_Drive_Scaling_Teleop = 0.6;
